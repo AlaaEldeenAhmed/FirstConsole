d540bc329e5e3cef3cb5e87751d711b871a93b02
alaa <alaaahmed8383@gmail.com>
2021-12-28 12:24:21 +0400

    add new folder for function classess

---
+++ Console/Calculator/Addtion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.Calculator
+{
+    public class Addtion
+    {
+        public static int Add(int NumberOne, int NumberTwo)
+        {
+            int sum = 0;
+            sum += NumberOne + NumberTwo;
+            return sum;
+        }
+    }
+}
